99131a7c9c44395abf1b083d319178a0a69144ed
srnatan <jonatanaradillas@gmail.com>
2024-04-16 20:09:09 +0200

    💡 Add private instances with conditional tags

--- ec2.tf
+++ ec2.tf
@@ -13,3 +13,22 @@ resource "aws_instance" "ec2_publica" {
   }
 }
 
+resource "aws_instance" "ec2_privada" {
+  count                  = length(aws_subnet.private_subnet)
+  ami                    = "ami-0a699202e5027c10d" 
+  instance_type          = "t2.micro"
+  key_name               = "ansible-keys"
+  subnet_id              = aws_subnet.private_subnet[count.index].id
+  vpc_security_group_ids = [aws_security_group.sg_instancias_privadas.id]
+
+  tags = merge(
+    {
+      Name         = "ec2-private-${count.index}"
+      owner        = "jonatan.dguez"
+      env          = "prod"
+      AnsibleGroup = "EC2_1"
+    },
+    count.index == 0 ? { GitGroup = "Si" } : {}
+  )
+}
+
