9b7e45490fdff7095202522bf80284ad4ab94bb2
srnatan <jonatanaradillas@gmail.com>
2024-04-18 20:29:35 +0200

    :sparkles: Add efs resource

---
+++ efs.tf
@@ -0,0 +1,15 @@
+resource "aws_efs_file_system" "ansible_efs" {
+  performance_mode = "generalPurpose"
+  encrypted        = false
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+}
+
+resource "aws_efs_mount_target" "efs_mt" {
+  count           = length(aws_subnet.private_subnet)
+  file_system_id  = aws_efs_file_system.ansible_efs.id
+  subnet_id       = aws_subnet.private_subnet[count.index].id
+  security_groups = [aws_security_group.sg_efs.id]
+}
