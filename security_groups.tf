d6532669df011132e355db0266389b58968991fe
srnatan <jonatanaradillas@gmail.com>
2024-04-18 20:33:21 +0200

    :sparkles: Update ingress rules

--- security_groups.tf
+++ security_groups.tf
@@ -18,17 +18,39 @@ resource "aws_security_group" "sg_bastion_host" {
   }
 }
 
+resource "aws_security_group" "sg_instancias_privadas" {
+  name        = "sg_instancias_privadas"
+  description = "Permiten puerto 5000 desde el ALB y SSH desde el Bastion Host"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_bastion_host.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
 resource "aws_security_group" "sg_efs" {
   name        = "sg_efs"
   description = "SG para EFS que permite acceso por el puerto 2049 desde las instancias privadas"
   vpc_id      = aws_vpc.main_vpc.id
 
-  #   ingress {
-  #     from_port       = 2049
-  #     to_port         = 2049
-  #     protocol        = "tcp"
-  #     security_groups = [aws_security_group.sg_instancias_privadas.id]
-  #   }
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_instancias_privadas.id]
+  }
 
   egress {
     from_port   = 0
@@ -43,12 +65,13 @@ resource "aws_security_group" "sg_rds" {
   description = "SG para RDS que permite acceso por el puerto 3306 desde las instancias privadas"
   vpc_id      = aws_vpc.main_vpc.id
 
-  #   ingress {
-  #     from_port       = 3306
-  #     to_port         = 3306
-  #     protocol        = "tcp"
-  #     security_groups = [aws_security_group.sg_instancias_privadas.id]
-  #   }
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_instancias_privadas.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -68,19 +91,6 @@ resource "aws_security_group" "sg_load_balancer" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "sg_instancias_privadas" {
-  name        = "sg_instancias_privadas"
-  description = "SG para Load Balancer que permite el acceso por el puerto 80 para todo el mundo"
-  vpc_id      = aws_vpc.main_vpc.id
-
   egress {
     from_port   = 0
     to_port     = 0
